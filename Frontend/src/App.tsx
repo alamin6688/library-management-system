cc4a2ba0d510e64d2cc944f963e35c3f3b053790
Alamin <alamin.kzs06@gmail.com>
2025-09-06 15:15:24 +0600

    Font Modified

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
+    <h1>Hello</h1>
       <div className="flex flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
