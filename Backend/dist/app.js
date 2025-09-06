7f42cd74ad9f32e9403441dccba58b02b7a22ca5
Alamin <alamin.kzs06@gmail.com>
2025-09-07 02:31:34 +0600

    Cors Added

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -5,8 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: [
+        "http://localhost:5173",
+        "https://library-management-mongoose.vercel.app",
+    ],
+}));
 app.use("/api", borrow_controller_1.libraryRoutes);
 // Centralized error handler
 app.use((err, req, res, next) => {
