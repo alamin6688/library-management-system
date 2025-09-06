a1c7c46b62c2d85550c7524ab3cfdc3279ca1165
Alamin <alamin.kzs06@gmail.com>
2025-09-06 15:59:09 +0600

    Logo added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
-      <div className="flex flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+   <Navbar></Navbar>
     </>
   );
 }
