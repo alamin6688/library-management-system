80d9ecde2617a8a6c506610fe9665cf7cab58e32
Alamin <alamin.kzs06@gmail.com>
2025-09-06 15:09:15 +0600

    Initial folder setup complete

---
+++ Backend/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import { libraryRoutes } from "./app/controllers/borrow.controller";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+
+app.use("/api", libraryRoutes);
+
+// Centralized error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    message: err.message || "Validation failed",
+    success: false,
+    error: err,
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome To Library Management App!!!");
+});
+
+export default app;
